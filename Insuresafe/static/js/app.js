e90d698d777549de1fdfed930fe74f914510dee2
mavacadoo <143280127+elfeyaoo@users.noreply.github.com>
2026-07-11 13:15:25 +0530

    done

--- Insuresafe/static/js/app.js
+++
@@ -1,6 +0,0 @@
-// static/js/app.js
-document.addEventListener('DOMContentLoaded', () => {
-  // Smooth scroll to flash
-  const alerts = document.querySelectorAll('.alert');
-  if (alerts.length) alerts[0].scrollIntoView({behavior:'smooth'});
-});
--- Insuresafe/test.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-print("YOLO imported successfully")
-import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-print(pytesseract.get_tesseract_version())
